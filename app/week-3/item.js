27f1c990aa190889accfc925def0ce5afb9e7db1
ZacharyC4 <z.louis.carter@outlook.com>
2024-09-25 13:28:48 -0600

    Week 3 Assignment-2
    Adding padding to make it look better
    (Might add more css)

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,8 @@
 export default function Item({name, quantity, category})
 {
     return (
-        <div className="text-indigo-200">
-            <ul className="border border-solid border-blue-300 max-w-sm mb-5 bg-indigo-800">
+        <div className="text-indigo-200 pl-4">
+            <ul className="border border-solid border-blue-300 max-w-sm mb-5 bg-indigo-800 py-2 px-3">
                 <li className="font-extrabold">{name}</li>
                 <li>Buy {quantity} in {category} </li>
             </ul>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="bg-indigo-950">
         <div>
-            <h1 className="text-3xl text-indigo-100 pb-4">Shopping List</h1>
+            <h1 className="text-3xl text-indigo-100 pb-4 pl-4">Shopping List</h1>
             <ItemList />
         </div>
     </main>
